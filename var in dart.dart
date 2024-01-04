6366263fc0114e286d3ba0b70de5efdbd05b9995
Md Sagor <130689825+sagorrrr11@users.noreply.github.com>
2024-01-05 00:21:51 +0600

    Create var in dart.dart

---
+++ var in dart.dart
@@ -0,0 +1,14 @@
+void main() {
+  var name = 'Md Sagor';
+  var birthYear = 2000;
+  var hisScore = 0.001;
+  var hisHouseAt = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': 'https://ugv.edu.bd/images/campus_images/1582007070.jpg'
+  };
+  print(
+      'Name: $name, Year of Birth: $birthYear, Score: $hisScore, Planet: ${hisHouseAt[1]}');
+}
+
+// output: Name: Md Sagor, Year of Birth: 2000, Score: 0.001, Planet: Saturn
